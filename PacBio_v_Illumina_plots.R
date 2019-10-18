5f18ee66ac7bbdb5e9355bea8fc61434b51a2d33
michellejlin <42459461+michellejlin@users.noreply.github.com>
2019-10-18 10:02:36 -0700

    slightly commented

--- PacBio_v_Illumina_plots.R
+++ PacBio_v_Illumina_plots.R
@@ -1,15 +1,12 @@
+# Currently this takes the path and one sample name. It will extract the sample's PacBio and Illumina columns 
+# from the allreads.csv file and output a scatterplot comparing PacBio vs. Illumina. Also outputs a zoomed in 
+# plot with range 0-10. 
+
 list.of.packages <- c("ggplot2", "grid", "nplr", "dplyr", "scales", "gridExtra", "RColorBrewer", "optparse")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
+suppressMessages(invisible(lapply(list.of.packages,library,character.only=T)))
 
-library(ggplot2)
-library(grid)
-library(nplr)
-library(dplyr)
-library(scales)
-library(gridExtra)
-library(RColorBrewer)
-library("optparse")
 
 cbbPalette <- c("#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
 
@@ -20,29 +17,45 @@ option_list <- list(make_option(c("-p", "--path"), type="character", default=NUL
 opt_parser <- OptionParser(option_list=option_list);
 opt <- parse_args(opt_parser)
 
-##Finding files in directory
 path <- opt$path
-#path <- "/Users/uwvirongs/Documents/Michelle/tprk_pipeline/testing"
+sample_name <- opt$sample_name
+
+#####
+
+## To run this script manually in R, uncomment the following lines. You do not need to change the preceding lines of path and sample_name,
+## but remember to recomment the lines if you want to run the script automatically in the pipeline.
+## path refers to the current working directory where the files and allreads_csv is.
+## sample_name refers to the sample you're currently working with for nicer-looking graphs. Make sure this name matches 
+## what was used in the allreads.csv (i.e. The sample name must be AS10 if PB_AS10_RelativeFreq is the column.)
+
+#path <- "/Users/uwvirongs/Documents/Michelle/tprk_pipeline/AS_files"
+#sample_name <- "AS10"
+
+## This script can also be run from the command line. 
+## Usage: rscript \path\to\Pacbio_v_Illumina_plots.R -p [path] -s [sample_name]
+
+#####
+
 allreads <- paste(path,"/allreads.csv", sep="")
 allreads_filtered <- paste(path,"/allreads_filtered.csv", sep="")
-sample_name <- opt$sample_name
-#sample_name <- "AS10"
 
 alldata <- read.csv(allreads,header=TRUE,sep=",",stringsAsFactors = FALSE)
 alldatafilt <- read.csv(allreads_filtered,header=TRUE,sep=",",stringsAsFactors = FALSE)
+# Grabs column names relevant to sample.
 illrf_col <- (paste("Ill_",sample_name,"_RelativeFreq",sep=""))
 pbrf_col <- (paste("PB_",sample_name,"_RelativeFreq",sep=""))
 pbcount_col <- (paste("PB_",sample_name,"_Count",sep=""))
 illcount_col <- (paste("Ill_",sample_name,"_Count",sep=""))
-
 vs_relative_freq <- select(alldata,Region,Read,illrf_col, pbrf_col)
 vs_relative_freq_filt <- select(alldatafilt,Region,Read,illrf_col, pbrf_col)
 
+# Gets r^2 value.
 model <- lm(alldata[[pbrf_col]] ~ alldata[[illrf_col]])
 anno <- paste("r^2 =",round(summary(model)$adj.r.squared,3))
 model_filt <- lm(alldatafilt[[pbrf_col]] ~ alldatafilt[[illrf_col]])
 anno_filt <- paste("r^2 =",round(summary(model_filt)$adj.r.squared,3))
 
+# Plots for non-filtered.
 plot <- ggplot(alldata,aes(x=alldata[[illrf_col]],y=alldata[[pbrf_col]],color=Region)) + 
   geom_point(size=2) + 
   geom_abline(linetype="dashed",color="grey") + theme_classic() + scale_colour_manual(values=cbbPalette) + ggtitle(sample_name) +
@@ -50,6 +63,7 @@ plot <- ggplot(alldata,aes(x=alldata[[illrf_col]],y=alldata[[pbrf_col]],color=Re
   theme(legend.position = "bottom",legend.box = "horizontal") + theme(legend.key.width = unit(0.1, "cm")) + 
   xlab(label="Illumina Frequency") + ylab(label="PacBio Frequency") + guides(color = guide_legend(nrow = 1))
 
+# Plots for filtered.
 plot_filt <- ggplot(alldatafilt,aes(x=alldatafilt[[illrf_col]],y=alldatafilt[[pbrf_col]],color=Region)) + 
   geom_point(size=2) + ggtitle(paste(sample_name,"Filtered")) + 
   geom_abline(linetype="dashed",color="grey") + theme_classic() + scale_colour_manual(values=cbbPalette) + 
@@ -62,14 +76,17 @@ g_legend<-function(a.gplot){
   legend <- tmp$grobs[[leg]]
   return(legend)}
 
+# Arranges non-filtered and filtered plots side by side.
 mylegend<-g_legend(plot)
 figure <- grid.arrange(arrangeGrob(plot + theme(legend.position="none"),
                                    plot_filt + theme(legend.position="none"),
                                    nrow=1),
                        mylegend, nrow=2,heights=c(10, 1))
+# Saves the plot in the Figures/ subdirectory.
 ggsave(paste(sample_name,"Illumina_vs_PacBio.pdf",sep="_"),path="Figures",plot=figure,width=6,height=3,units="in")
 
 
+# Creates plot zoomed in on range 0-10.
 lim10plot <- ggplot(alldata, aes(x=alldata[[illrf_col]],y=alldata[[pbrf_col]],color=Region)) + xlim(0,10) + ylim(0,10) + 
   geom_point(size=2) + xlab(label="Illumina Frequency") + ylab(label="PacBio Frequency") + 
   geom_abline(linetype="dashed", color = "grey") + theme_classic() + ggtitle(sample_name) + 
@@ -81,6 +98,8 @@ lim10plot_filt <- ggplot(alldatafilt, aes(x=alldatafilt[[illrf_col]],y=alldatafi
 figurelim10 <- grid.arrange(arrangeGrob(lim10plot + theme(legend.position="none"),
                                    lim10plot_filt + theme(legend.position="none"), nrow=1),
                        mylegend, nrow=2,heights=c(10, 1))
+# Saves the plot in the Figures/ subdirectory.
 ggsave(paste(sample_name,"Illumina_vs_PacBio_lim0-10.pdf",sep="_"),path="Figures",plot=figurelim10,width=6,height=3,units="in")
+
 file.exists("Rplots.pdf")
 file.remove("Rplots.pdf")
