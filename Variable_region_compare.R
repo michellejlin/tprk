e4f52f227770f3557567542f50eb0f498dbe71d3
michellejlin <42459461+michellejlin@users.noreply.github.com>
2019-10-18 10:09:39 -0700

    slightly commented

--- Variable_region_compare.R
+++ Variable_region_compare.R
@@ -1,3 +1,9 @@
+# Generates dot-line plots for comparing the variable regions between two samples.
+# Currently takes the path and goes through the allreads.csv.
+
+# TODO: Find a better way to organize these so they don't generate a billion files.
+# TODO: Also maybe do it for the filtered data too.
+
 list.of.packages <- c("ggplot2", "grid", "nplr", "plyr", "dplyr", "scales", "gridExtra", "RColorBrewer", "optparse","randomcoloR", "cowplot",
                       "tidyr", "tibble")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
@@ -9,11 +15,24 @@ option_list <- list(make_option(c("-p", "--path"), type="character", default=NUL
 opt_parser <- OptionParser(option_list=option_list);
 opt <- parse_args(opt_parser)
 
-##Finding files in directory
 path <- opt$path
-#path <- "/Users/uwvirongs/Documents/Michelle/tprk_pipeline/testing"
+
+#####
+
+## To run this script manually in R, uncomment the following lines. You do not need to change the preceding line of path
+## but remember to recomment the lines if you want to run the script automatically in the pipeline.
+## path refers to the folder your metadata.csv and sequencing files (.fastq) are.
+
+#path <- "/Users/uwvirongs/Documents/Michelle/tprk_pipeline/AS_files"
+
+## This script can also be run from the command line.
+## Usage: rscript \path\to\Variable_region_compare.R -p [path]
+
+#####
+
 allreads <- paste(path,"/allreads_filtered.csv", sep="")
 allreads_filtered <- paste(path,"/allreads_filtered.csv", sep="")
+# Grabs sample names from metadata.csv.
 metadata_csv <- paste(path,"/metadata.csv", sep="")
 metadata = read.csv(metadata_csv)
 sample_names = as.character(metadata$SampleName)
@@ -21,11 +40,14 @@ setwd(path)
   
 alldata <- read.csv(allreads,header=TRUE,sep=",",stringsAsFactors = FALSE)
 
+# Loops through and generates variable region comparisons for all the combinations of files.
+# This means a lot of files if list is long.
 for (i in 1:(length(sample_names) - 1)) {
   for (j in (i+1):(length(sample_names))) {
     print(paste("Generating figure for ",sample_names[i]," and ",sample_names[j],"...",sep=""))
     rfcol <- paste("Ill_",sample_names[i],"_RelativeFreq",sep = "")
     rfcol2 <- paste("Ill_",sample_names[j],"_RelativeFreq",sep = "")
+
     commondfIllumina <- select(alldata,Region,Read,rfcol,rfcol2)
     commondfIllumina <- filter(commondfIllumina,!((commondfIllumina[[rfcol]] == 0) & (commondfIllumina[[rfcol2]] == 0)))
     sortedIllumina <- commondfIllumina[order(commondfIllumina$Region,-commondfIllumina[[rfcol]],-commondfIllumina[[rfcol2]]),]
