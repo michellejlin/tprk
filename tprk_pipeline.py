9308c6b4380bdd08852be62fb993ff9ff9de1a10
michellejlin <42459461+michellejlin@users.noreply.github.com>
2020-01-22 21:15:23 -0800

    changes filtering for only heatmap
    Previously, if two samples shared the same read, both samples would be included even if one did not pass count/rf filter. Now, this only applies to the heatmap, where we want to see read frequency across all samples. For everything else, anything that doesn't pass the count/rf filter will be eliminated.

--- tprk_pipeline.py
+++ tprk_pipeline.py
@@ -7,7 +7,7 @@ import regex
 
 # Filters allreads.csv and outputs allreads_filtered.csv based on flags -f and -c.
 # (Default relative freq is 0.2 and count is 5.) Thrown away reads are put into allreads_thrownout.csv.
-def filter_table(relative_freq_filter, count_filter, table):
+def filter_table(relative_freq_filter, count_filter, table, is_heatmap):
 	line_num = 0
 	table_csv = open(table, "r")
 	table_lines = table_csv.readlines()
@@ -19,41 +19,74 @@ def filter_table(relative_freq_filter, count_filter, table):
 		# Ignores the first line and initializes files.
 		if line_num == 0:
 			first_line = line
-			table_filtered = open(table.split(".csv")[0] + "_filtered.csv", "w+")
+			if(is_heatmap):
+				print("Generating allreads_filtered_heatmap.csv file...")
+				table_filtered = open(table.split(".csv")[0] + "_filtered_heatmap.csv", "w+")
+			else:
+				table_filtered = open(table.split(".csv")[0] + "_filtered.csv", "w+")
 			table_filtered.write(line + "\n")
-			table_reads_thrownout.write(line + "\n")
 		# Filters lines based on specified values. Checks that each sample passes both the relative frequency filter
 		# and the count filter. Writes passed lines to alldata_filtered.csv, and thrown away lines to alldata_thrownout.csv.
 		else:
 			line_parts = line.split(",")
 			relative_freq_check = False
 			count_check = False
-			for index, part in enumerate(line_parts):
-				# Even indexes should be relative frequencies
-				if ((index % 2) == 0) and index != 0 and index != 1 and part != "NA" and float(part) >= relative_freq_filter:
-					relative_freq_check = True
-				# Odd indexes should be counts
-				if ((index % 2 == 1)) and index != 0 and index != 1 and part != "NA" and float(part) >= count_filter:
-					count_check = True
-				# For allreads_filtered.csv, we want to change anything under the count filter to NA.
-				if ("allreads" in table) and ((index % 2 == 1)) and index != 0 and part != "NA" and index != 1 and float(part) < count_filter:
-					line_parts[index] = "NA"
-					line_parts[index - 1] = "NA"
 
 			newline = line_parts[0] + "," + line_parts[1]
+			newline_hm = line_parts[0] + "," + line_parts[1]
+
+			# print(line_parts[1])
+
+			for index, part in enumerate(line_parts):
+				if (index != 0 and index != 1):
+					# For non-heatmap, we don't want to include parts that don't pass filters, even if there are other samples with the same read that do.
+					if (is_heatmap == False):
+						if (index % 2 == 1):
+							# print(part)
+							# print(line_parts[index-1])
+							if (part != "NA" and (float(part) >= count_filter) and (float(line_parts[index - 1]) >= relative_freq_filter)):
+								newline = newline + "," + line_parts[index - 1] + "," + part
+								relative_freq_check = True
+								count_check = True
+								# print(index)
+								# print(newline)
+							else:
+								newline = newline + ",NA,NA"
+								# print(newline)
+					# Heatmap only! Check if there are samples with the same read name that do have the read, in which case we keep the whole line.
+					# Even indexes should be relative frequencies
+					else:					
+						if (part != "NA" and (index % 2) == 0) and float(part) >= relative_freq_filter:
+							relative_freq_check = True
+						# Odd indexes should be counts
+						if (part != "NA" and (index % 2 == 1)) and float(part) >= count_filter:
+							count_check = True
+						# For allreads_filtered.csv, we want to change anything under the count filter to NA.
+						if (part != "NA" and ("allreads" in table) and (index % 2 == 1) and float(part) < count_filter):
+							line_parts[index] = "NA"
+							line_parts[index - 1] = "NA"
+
+			# Create the whole line again for the heatmap.
 			for index, part in enumerate(line_parts):
 				if(index != 0 and index != 1):
-					newline = newline + "," + part
+					newline_hm = newline_hm + "," + part
 
-			# Passes the filter if any one of the samples pass both the relative filter and count filter
+			# Passes the filter if any one of the samples pass both the relative filter and count filter, only for heatmap
 			if relative_freq_check and count_check:
-				table_filtered.write(newline + "\n")
-			else:
+				if is_heatmap:
+					table_filtered.write(newline_hm + "\n")
+				else:
+					table_filtered.write(newline + "\n")
+			elif not is_heatmap:
 				table_reads_thrownout.write(newline + "\n")
 
 		line_num = line_num + 1
 	table_filtered.close()
-	sort_command = "sort -t, -k1,1 -k3,3nr < " + table.split(".csv")[0] + "_filtered.csv > a.tmp && mv a.tmp " + table.split(".csv")[0] + "_filtered.csv"
+
+	if is_heatmap:
+		sort_command = "sort -t, -k1,1 -k3,3nr < " + table.split(".csv")[0] + "_filtered_heatmap.csv > a.tmp && mv a.tmp " + table.split(".csv")[0] + "_filtered_heatmap.csv"
+	else:
+		sort_command = "sort -t, -k1,1 -k3,3nr < " + table.split(".csv")[0] + "_filtered.csv > a.tmp && mv a.tmp " + table.split(".csv")[0] + "_filtered.csv"
 	subprocess.call(sort_command, shell=True)
 
 
@@ -83,7 +116,7 @@ def bokeh_freq_plot(script_path, cur_dir, new_dir, relative_freq_filter, count_f
 			" -t " + sample_name + " -o " + new_dir + " " + svg_flag, shell=True)
 
 			# Filters the current final_data.csv.
-			filter_table(relative_freq_filter, count_filter, cur_dir + "/" + sample_file)
+			filter_table(relative_freq_filter, count_filter, cur_dir + "/" + sample_file, False)
 			# Recalculates new relative frequencies now that it's filtered.
 			subprocess.call("rscript " + script_path + "/recalculate_frequency.R -d " + cwd + " -f " + 
 				cur_dir + "/" + sample_file.split(".csv")[0] + "_filtered.csv", shell=True)
@@ -175,8 +208,13 @@ if __name__ == '__main__':
 		pacbio_flag + " " + illumina_flag, shell=True)
 
 	# Creates allreads_filtered.csv and recalculates the relative frequencies.
-	filter_table(relative_freq_filter, count_filter, cwd + "/allreads.csv")
+	filter_table(relative_freq_filter, count_filter, cwd + "/allreads.csv", False)
 	subprocess.call("rscript " + script_path + "/recalculate_frequency.R -d " + cwd + " -f " + cwd + "/allreads_filtered.csv", shell=True)
+
+	# Creates allreads_filtered_heatmap.csv and recalculates the relative frequencies. This csv includes samples under the count/relative freq filters
+	# if another sample shares the same read. 
+	filter_table(relative_freq_filter, count_filter, cwd + "/allreads.csv", True)
+	subprocess.call("rscript " + script_path + "/recalculate_frequency.R -d " + cwd + " -f " + cwd + "/allreads_filtered_heatmap.csv", shell=True)
 
 	# Creates a subfolder in current directory called Figures for the figures to go into.
 	figure_output_path = cwd + "/Figures"
@@ -215,7 +253,7 @@ if __name__ == '__main__':
 
 	# Creates heatmap and combined relative frequency plots off filtered all data (allreads_filtered.csv).
 		print("Generating all data heatmap and relative frequency plots...")
-		subprocess.call("python " + script_path + "/alldata_visualizer.py allreads_filtered.csv metadata.csv",shell=True)
+		subprocess.call("python3 " + script_path + "/alldata_visualizer_alex.py allreads_filtered.csv metadata.csv",shell=True)
 
 	# Creates a tree based off the filtered cumulative data (alldata_filtered.csv). 
 	# Currently no rooting takes place (until somebody figures out how to automate that).
